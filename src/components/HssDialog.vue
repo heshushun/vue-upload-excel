1e89b26e91cf4613607cfd955517c2c4b593fbf3
heshushun <1060067034@qq.com>
2022-04-24 01:56:37 +0800

    [upd]弹窗组件添加

---
+++ src/components/HssDialog.vue
@@ -0,0 +1,62 @@
+<template>
+    <el-dialog title="详细信息" :visible.sync="dialogVisible" :before-close="cancelDialog" >
+        <el-form class="form">
+            <template v-for="(info, key) in dialogInfo">
+                <el-form-item :label="'[ '+key+' ] :  '" :key="key">
+                    {{info}}
+                </el-form-item>
+            </template>
+        </el-form>
+        <div slot="footer" class="dialog-footer">
+            <el-button @click="cancelDialog">关 闭</el-button>
+            <el-button type="primary" @click="cancelDialog">确 定</el-button>
+        </div>
+    </el-dialog>
+</template>
+
+<script>
+    export default {
+        name: "HssDialog",
+        components: {},
+        props: {
+            dialogVisible: {
+                type: Boolean,
+                default: () => false
+            },
+            dialogInfo: {
+                type: Object,
+                default: () => {}
+            }
+        },
+        watch: {
+            //监听弹窗显示，可以用来写请求接口
+            dialogVisible: function(newVal, oldVal) {
+                if (newVal) {
+                    console.log("dialogVisible newVal: ", newVal);
+                    console.log("dialogVisible oldVal: ", oldVal);
+                }
+            }
+        },
+        data() {
+            return {
+            };
+        },
+        created() {},
+        mounted() {},
+        methods: {   //修改父组件传过来的值
+            cancelDialog() {
+                this.$emit("update:dialogVisible", false);
+            }
+        }
+    };
+</script>
+<style scoped>
+    .form{
+        color: #dc6a66;
+        background: #eee;
+        padding: 0 10px;
+    }
+    .dialog-footer{
+        text-align: center;
+    }
+</style>
